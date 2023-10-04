0d2f53b95657a7bd2d40f9cca3c56b572c80e09b
Logan Fossenier <logan.fossenier@gmail.com>
2023-10-04 12:46:14 -0600

    feat: update class to only contain pit data

--- mancalculations.py
+++ mancalculations.py
@@ -1,37 +1,46 @@
+PIT_COUNT = 6
+
+
 def main():
     board = MancalaBoard()
     # The game will be broken into Player 0 and Player 1 turns
-    p_turn = 0
-    game_winner = None
-    try:
-        while True:
-            pit_choice = input("Select a pit to move: ")
-            game_winner = board.make_move(p_turn, pit_choice)
-            p_turn = 1 - p_turn
-            if game_winner is not None:
-                break
-            pass
-    except KeyboardInterrupt:
-        print("Exiting game...")
-        return
-
-
-def take_turn(p1_turn):
+    # p_turn = 0
+    # game_winner = None
+    # try:
+    #     while True:
+    #         pit_choice = input("Select a pit to move: ")
+    #         game_winner = board.make_move(p_turn, pit_choice)
+    #         p_turn = 1 - p_turn
+    #         if game_winner is not None:
+    #             break
+    #         pass
+    # except KeyboardInterrupt:
+    #     print("Exiting game...")
     return
+
+
+def make_move(self, player, pit_index):
+    # Implement the game logic here to update the board state after a move
+    pass
+
+
+def is_game_over(self):
+    # Check if the game is over according to the rules
+    pass
+
+
+def score(self):
+    # Determine the winner or declare a tie
+    pass
 
 
 class MancalaBoard:
     def __init__(self):
-        self.pits[[4, 4, 4, 4, 4, 4, 0], [4, 4, 4, 4, 4, 4, 0]]
+        self.p1_pits = [4, 4, 4, 4, 4, 4]
+        self.p2_pits = [4, 4, 4, 4, 4, 4]
+        self.p1_store = 0
+        self.p2_store = 0
 
-    def make_move(self, player, pit_index):
-        # Implement the game logic here to update the board state after a move
-        pass
 
-    def is_game_over(self):
-        # Check if the game is over according to the rules
-        pass
-
-    def score(self):
-        # Determine the winner or declare a tie
-        pass
+if __name__ == "__main__":
+    main()
