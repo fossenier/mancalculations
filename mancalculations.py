39996be80c8d66164b61ac902c0bdadf43681f27
Logan Fossenier <logan.fossenier@gmail.com>
2023-10-06 20:35:05 -0600

    feat: update move functionality

--- mancalculations.py
+++ mancalculations.py
@@ -2,29 +2,20 @@ PIT_COUNT = 6
 
 
 def main():
-    run_game()
-    # The game will be broken into Player 0 and Player 1 turns
-    # p_turn = 0
-    # game_winner = None
-    # try:
-    #     while True:
-    #         pit_choice = input("Select a pit to move: ")
-    #         game_winner = board.make_move(p_turn, pit_choice)
-    #         p_turn = 1 - p_turn
-    #         if game_winner is not None:
-    #             break
-    #         pass
-    # except KeyboardInterrupt:
-    #     print("Exiting game...")
+    print("Mancala is starting")
+    p1_score, p2_score = run_game()
     return
 
 
-def make_move(self, player, pit_index):
+def move_pit(board, player_turn, pit_selection):
     # Implement the game logic here to update the board state after a move
+    rock_count, board.p_pits[player_turn][pit_selection] = board.p_pits[player_turn][pit_selection], 0
+    for i in range(rock_count):
+        active_pit = (pit_selection + i + 1)
     pass
 
 
-def is_game_over(self):
+def is_game_over(board):
     # Check if the game is over according to the rules
     pass
 
@@ -38,20 +29,20 @@ def run_game():
     game_is_over = False
     player_turn = 0
     board = MancalaBoard()
-    print("Mancala is starting")
     print(board.p1_pits, board.p1_store, board.p2_pits, board.p2_store)
     while not game_is_over:
-        pit_selection = take_turn(board, player_turn)
+        pit_selection = select_pit(board, player_turn)
+        move_pit(board, player_turn, pit_selection)
+        game_is_over = is_game_over(board)
         player_turn = 1 - player_turn
-    return
+    return score(board)
 
 
-def take_turn(board, player_turn):
-    print("aaaa")
+def select_pit():
     pit_selection = -1
     while pit_selection < 0 or pit_selection > PIT_COUNT:
         try:
-            pit_selection = int(input("input: "))
+            pit_selection = int(input("Enter a valid pit (0-6): "))
         except TypeError:
             print("Sorry, that was an invalid input, please enter an integer [0, 6]")
     return pit_selection
@@ -59,10 +50,8 @@ def take_turn(board, player_turn):
 
 class MancalaBoard:
     def __init__(self):
-        self.p1_pits = [4, 4, 4, 4, 4, 4]
-        self.p2_pits = [4, 4, 4, 4, 4, 4]
-        self.p1_store = 0
-        self.p2_store = 0
+        self.p_pits = [[4, 4, 4, 4, 4, 4], [4, 4, 4, 4, 4, 4]]
+        self.p_store = [0, 0]
 
 
 if __name__ == "__main__":
