8463d458453c92757d7629cbedc5e05fcd3366d2
Logan Fossenier <logan.fossenier@gmail.com>
2023-10-16 09:31:19 -0600

    feat: update user messages on same line with waits

--- constants.py
+++ constants.py
@@ -2,6 +2,9 @@
 This is the list of constants for my Mancala game.
 """
 
+VERTICAL_OFFSET_OF_ERROR = 10
+VERTICAL_OFFSET_OF_PROMPT = 9
+MESSAGE_WAIT = 1.5
 PIT_WIDTH = 3
 PLAYER_PIT_COUNT = 6
 STORE_WIDTH = 5
--- mancal.py
+++ mancal.py
@@ -3,8 +3,12 @@ This is the frontend code for my Mancala game.
 It runs the game and provides the user interface.
 """
 
+from constants import (
+    VERTICAL_OFFSET_OF_ERROR,
+    VERTICAL_OFFSET_OF_PROMPT,
+    MESSAGE_WAIT,
+)
 from culations import check_game_over, MancalaBoard, move_rocks, score_game, steal_rocks
-import curses as curses
 from visuals import (
     draw_blank,
     draw_header,
@@ -12,6 +16,8 @@ from visuals import (
     draw_pit_selection,
     draw_message,
 )
+import curses as curses
+import time as time
 
 
 def main(stdscr):
@@ -56,25 +62,33 @@ def get_player_move(stdscr):
     pit_selection = -1
     while pit_selection < 1 or 6 < pit_selection:
         # show cursor and clear previous prompt
-        draw_blank(stdscr, 9, 0, 1)
+        draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 1)
         curses.curs_set(1)
         # get player input
-        input = draw_pit_selection(stdscr, 9, 0)
+        input = draw_pit_selection(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0)
         try:
             pit_selection = int(input)
             # display error if number is out of range
             if pit_selection < 1 or 6 < pit_selection:
-                draw_blank(stdscr, 10, 0, 1)
+                draw_blank(stdscr, VERTICAL_OFFSET_OF_ERROR, 0, 1)
                 draw_message(
-                    stdscr, 10, 0, "Invalid number. Must enter 1, 2, 3, 4, 5, or 6."
+                    stdscr,
+                    VERTICAL_OFFSET_OF_ERROR,
+                    0,
+                    "Invalid number. Must enter 1, 2, 3, 4, 5, or 6.",
                 )
         # display error if input is not a number
         except ValueError:
-            draw_blank(stdscr, 10, 0, 1)
-            draw_message(stdscr, 10, 0, "Invalid input. Please enter a number.")
+            draw_blank(stdscr, VERTICAL_OFFSET_OF_ERROR, 0, 1)
+            draw_message(
+                stdscr,
+                VERTICAL_OFFSET_OF_ERROR,
+                0,
+                "Invalid input. Please enter a number.",
+            )
     # hide cursor and clear prompt and potential error message
     curses.curs_set(0)
-    draw_blank(stdscr, 9, 0, 2)
+    draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 2)
     return pit_selection - 1
 
 
@@ -87,18 +101,32 @@ def run_turn(stdscr, board, player_turn):
         `board` (`MancalaBoard`): Current game state.\n
         `player_turn` (`integer`): Current player turn.
     """
+    is_steal = False
     pit_selection = get_player_move(stdscr)
     active_side, active_pit = move_rocks(board, player_turn, pit_selection)
     draw_mancala_board(stdscr, board, 3, 0)
     # give the player another turn if they landed in their store
     if active_pit == -1:
-        draw_message(stdscr, 11, 0, "Nice one, take another turn!")
+        draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 1)
+        draw_message(
+            stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, "Nice one, take another turn!"
+        )
+        time.sleep(MESSAGE_WAIT)
+        draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 1)
         run_turn(stdscr, board, player_turn)
     # if the active side is the player's, check if a steal occurs
     else:
         is_steal = steal_rocks(board, active_side, player_turn, active_pit)
         if is_steal:
-            draw_message(stdscr, 11, 0, "Steal!")
+            draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 1)
+            draw_message(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, "Steal!")
+            time.sleep(MESSAGE_WAIT)
+            draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 1)
+    if not is_steal:
+        draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 1)
+        draw_message(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, "Next player's turn.")
+        time.sleep(MESSAGE_WAIT)
+        draw_blank(stdscr, VERTICAL_OFFSET_OF_PROMPT, 0, 1)
 
 
 if __name__ == "__main__":
